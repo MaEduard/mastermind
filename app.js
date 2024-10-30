5ef7cd5dfdd1c84cb6790a90f7efc5eea85b94fe
MaEduard <eduard.ma6@gmail.com>
2024-10-30 14:48:35 +0100

    Implemented the server through Node.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var indexRouter = require("./routes/index");
 var messages = require("./public/javascripts/messages");
 var gameStatus = require("./statTracker");
 var Game = require("./game");
+const e = require("express");
 
 var port = process.argv[2];
 var app = express();
@@ -58,7 +59,7 @@ wss.on("connection", function connection(ws) {
 
     let informClientPlayerType = "";
 
-    if (playerType == A) {
+    if (playerType == 'A') {
         informClientPlayerType = messages.S_PLAYER_A;
     }
     else {
@@ -70,17 +71,14 @@ wss.on("connection", function connection(ws) {
      */
     con.send(informClientPlayerType);
 
-    /*
-     * client B receives the target word (if already available)
-     */
-    if (playerType == "B" && currentGame.getColorCode() != null) {
-        let msg = messages.O_TARGET_WORD;
-        msg.data = currentGame.getColorCode();
-        con.send(JSON.stringify(msg));
+    if (playerType == 'B' && currentGame.getColorCode() != null) {
+        let sendCode = messages.O_TARGET_WORD;
+        sendCode.data = currentGame.getColorCode();
+        con.send(JSON.stringify(sendCode));
     }
 
     /*
-     * once we have two players, there is no way back;
+     * once we have two players, there is no way back; 
      * a new game object is created;
      * if a player now leaves, the game is aborted (player is not preplaced)
      */
@@ -98,18 +96,23 @@ wss.on("connection", function connection(ws) {
         let oMsg = JSON.parse(message);
 
         let gameObj = websockets[con.id];
-        let isPlayerA = gameObj.playerA == con ? true : false;
+        fromPlayerA = true;
 
-        if (isPlayerA) {
+        if (gameObj.playerB == con) {
+            fromPlayerA = false;
+        }
+
+        if (fromPlayerA) {
             /*
-             * player A cannot do a lot, just send the target word;
-             * if player B is already available, send message to B
-             */
+            * player A cannot do a lot, just send the target code;
+            * if player B is already available, send message to B
+            */
             if (oMsg.type == messages.T_TARGET_WORD) {
-                gameObj.setWord(oMsg.data);
+                gameObj.setColorCode(oMsg.data);
 
                 if (gameObj.hasTwoConnectedPlayers()) {
-                    gameObj.playerB.send(message);
+                    let msg = JSON.parse(message);
+                    gameObj.playerB.send(JSON.stringify(msg));
                 }
             }
         } else {
@@ -118,7 +121,8 @@ wss.on("connection", function connection(ws) {
              * this guess is forwarded to A
              */
             if (oMsg.type == messages.T_MAKE_A_GUESS) {
-                gameObj.playerA.send(message);
+                let msg = JSON.parse(message);
+                gameObj.playerA.send(JSON.stringify(msg));
                 gameObj.setStatus("ROW GUESSED");
             }
 
@@ -127,47 +131,26 @@ wss.on("connection", function connection(ws) {
              */
             if (oMsg.type == messages.T_GAME_WON_BY) {
                 gameObj.setStatus(oMsg.data);
+                let msg = JSON.parse(message);
+                gameObj.playerA.send(JSON.stringify(msg));
+                console.log("The game has been won by:" + ' ' + oMsg.data);
                 //game was won by somebody, update statistics
                 gameStatus.gamesCompleted++;
             }
         }
     });
 
-    con.on("close", function (code) {
-        /*
-         * code 1001 means almost always closing initiated by the client;
-         * source: https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
-         */
+    con.on("close", function () {
         console.log(con.id + " disconnected ...");
+        let gameObj = websockets[con.id];
 
-        if (code == "1001") {
-            /*
-             * if possible, abort the game; if not, the game is already completed
-             */
-            let gameObj = websockets[con.id];
-
-            if (gameObj.isValidTransition(gameObj.gameState, "ABORTED")) {
-                gameObj.setStatus("ABORTED");
-                gameStatus.gamesAborted++;
-
-                /*
-                 * determine whose connection remains open;
-                 * close it
-                 */
-                try {
-                    gameObj.playerA.close();
-                    gameObj.playerA = null;
-                } catch (e) {
-                    console.log("Player A closing: " + e);
-                }
-
-                try {
-                    gameObj.playerB.close();
-                    gameObj.playerB = null;
-                } catch (e) {
-                    console.log("Player B closing: " + e);
-                }
+        if (con.id % 2 == 0) {
+            if (gameObj.hasTwoConnectedPlayers()) {
+                gameObj.playerB.send(messages.S_GAME_ABORTED);
             }
+        }
+        else {
+            gameObj.playerA.send(messages.S_GAME_ABORTED);
         }
     });
 });
